485a3eca9cb63f28ba81f03ec3f45161e0c8c932
HliebPantyshenko <pantushenko11@gmail.com>
2023-07-07 16:21:58 +0300

    Created Menu class, Created CeasedCypher class. Created choise from user to encode and decode method

---
+++ src/CeasedCypher.java
@@ -0,0 +1,11 @@
+public class CeasedCypher {
+    public static void encode(String fileName){
+        System.out.println("Encode!");
+        new Menu().itemToExit();
+    }
+
+    public static void decode(String fileName){
+        System.out.println("Decode!");
+        new Menu().itemToExit();
+    }
+}
