a97724c748a1aa58ad377be83e898b49eeef32d6
HliebPantyshenko <pantushenko11@gmail.com>
2023-07-07 16:35:28 +0300

    Fixed exit method

--- src/CeasedCypher.java
+++ src/CeasedCypher.java
@@ -1,11 +1,11 @@
 public class CeasedCypher {
     public static void encode(String fileName){
         System.out.println("Encode!");
-        new Menu().itemToExit();
+        new Menu().run();
     }
 
     public static void decode(String fileName){
         System.out.println("Decode!");
-        new Menu().itemToExit();
+        new Menu().run();
     }
 }
